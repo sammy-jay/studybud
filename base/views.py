fc6b0188899430c3631fe2ce53314cb331190b6d
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-23 15:07:31 +0000

    added some more fns

--- base/views.py
+++ base/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Let\'s learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    context = {}
+    for room in rooms:
+        if room['id'] == int(pk):
+            context['room'] = room
+            break
+    return render(request, 'base/room.html', context)
