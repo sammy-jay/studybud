5d796403cc1c8329612623d04a582ed3de3bf718
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-23 15:07:07 +0000

    added the Room model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    #host = 
+    #topic = 
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    # participants = 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
